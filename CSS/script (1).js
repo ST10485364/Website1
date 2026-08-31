e9cc6fe598966f8cd55c547bc7dbb2f8e2488980
ST10485364 <ST10485364@rcconnect.edu.za>
2026-08-31 13:57:53 +0200

    1stcommit

---
+++ CSS/script (1).js
@@ -0,0 +1,56 @@
+
+// Lightbox functionality
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const captionText = document.getElementById("caption");
+const closebtn = document.getElementsByClassName("close")[0];
+
+if (lightbox && lightboxImg && captionText && closebtn) {
+    document.querySelectorAll(".gallery-img").forEach(img => {
+        img.addEventListener("click", function() {
+            lightbox.style.display = "block";
+            lightboxImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        });
+    });
+
+    closebtn.addEventListener("click",() => {
+        lightbox.style.display = "none";
+    });
+
+    window.addEventListener("click", (event) => {
+        if (event.target === lightbox) {
+            lightbox.style.display = "none";
+        }
+    });
+
+    function closeLightbox() {
+        lightbox.style.display = "none";
+}
+}
+
+// Search functionality
+const searchInput = document.getElementById('searchInput');
+const productList = document.getElementById('productList');
+
+if (searchInput && productList) {
+    const products = productList.getElementsByTagName('li');
+
+    searchInput.addEventListener('input', function() {
+        const filter = searchInput.value.toLowerCase();
+        for (let i = 0; i < products.length; i++) {
+            const item = products[i].textContent.toLowerCase();
+            item.includes(filter)
+                ? products[i].classList.remove('hide')
+                : products[i].classList.add('hide');
+        }
+    });
+}
+
+
+
+
+
+
+
